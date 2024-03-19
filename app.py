a7a287457d63bb9583ae1bae8ae550ba2cda0611
Emily Tran <Emily.tran.1995@gmail.com>
2024-03-19 09:30:25 -0700

    change

--- app.py
+++ app.py
@@ -10,16 +10,22 @@ model_name = "Kaludi/Reviews-Sentiment-Analysis"
 model = AutoModelForSequenceClassification.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == 'POST':
-        text = request.form['text']
+    if request.method == "POST":
+        text = request.form["text"]
         inputs = tokenizer(text, return_tensors="pt")
         outputs = model(**inputs)
         probs = F.softmax(outputs.logits, dim=-1)
-        sentiment = 'Positive' if probs[0][1] > probs[0][0] else 'Negative'
-        return render_template('index.html', sentiment=sentiment, text=text)
-    return render_template('index.html', sentiment='', text='')
+        # sentiment = 'Positive' if probs[0][1] > probs[0][0] else 'Negative'
+        if probs[0][1] > probs[0][0]:
+            sentiment = probs[0][1].item()
+        else:
+            sentiment = -probs[0][0].item()
+        return render_template("index.html", sentiment=sentiment, text=text)
+    return render_template("index.html", sentiment="", text="")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
